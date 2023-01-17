aee3e99d824e891fafd969168f854893faa423eb
RafMat159 <rmatusiak159@gmail.com>
2023-01-17 23:35:44 +0100

    first step - hibernate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
-module pk.pwjj.klient {
+open module pk.pwjj.klient {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
+    requires hibernate.jpa;
+    requires java.persistence;
 
-    opens pk.pwjj.klient to javafx.fxml;
     exports pk.pwjj.klient;
 }
---
+++ src/main/java/pk/pwjj/entity/Ranking.java
@@ -0,0 +1,60 @@
+package pk.pwjj.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ranking")
+public class Ranking {
+
+    @Id
+    @Column(name = "user_id")
+    private Integer id;
+
+    @Column(name = "game_lost")
+    private Integer gameLost;
+
+    @Column(name = "game_win")
+    private Integer gameWin;
+
+    /**
+     * Hibernate (JPA) needs it.
+     * */
+    @SuppressWarnings("unused")
+    public Ranking() {
+    }
+
+    public Ranking(Integer gameLost, Integer gameWin, User user) {
+        this.gameLost = gameLost;
+        this.gameWin = gameWin;
+        this.user = user;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Integer getGameLost() {
+        return gameLost;
+    }
+
+    public void setGameLost(Integer gameLost) {
+        this.gameLost = gameLost;
+    }
+
+    public Integer getGameWin() {
+        return gameWin;
+    }
+
+    public void setGameWin(Integer gameWin) {
+        this.gameWin = gameWin;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
